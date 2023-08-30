22133353fa248bec3cb8aefcbc7d709b90bc6f6f
siripothuraju <143597594+siripothuraju@users.noreply.github.com>
2023-08-30 15:40:59 +0530

    Add files via upload

---
+++ booksdb.sql
@@ -0,0 +1,10 @@
+create database booksdb;
+use booksdb;
+create table Books(book_title varchar(60),Author varchar(25),genre varchar(20),publication_year int(20),price int(20));
+insert into books values("let C","Yashavant kanektar","C language",2017,369);
+insert into books values("network programming","lewis van winkle","network programming",2019,2888);
+insert into books values("python programming","reema thareja","python",2023,593);
+update books set price=550 where price=369;
+select * from books;
+delete from books where book_title="let C";
+select * from books;
